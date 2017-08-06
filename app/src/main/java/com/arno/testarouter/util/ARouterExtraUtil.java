df0cea6d757c1af0ef24ffdd51098de2379f3d80
arno <moomogn@sina.cn>
2017-08-06 14:15:55 +0800

    增加 Util 说明

--- app/src/main/java/com/arno/testarouter/util/ARouterExtraUtil.java
+++ app/src/main/java/com/arno/testarouter/util/ARouterExtraUtil.java
@@ -8,7 +8,14 @@ public class ARouterExtraUtil {
 
     private ARouterExtraUtil(){}
 
-    public static boolean checkTag(int position){
+    /**
+     * ARouter 标记的页面，可以设置一个 extra 值作为 32 个开关
+     * 这个方法用于获取开关的值，需要通过 position 获取开关位数的掩码，与操作后查看是否等于零
+     * @param extra extra 值
+     * @param position 开关位置
+     * @return 开关状态
+     */
+    public static boolean checkTag(int extra,int position){
 
         return false;
     }
